584737eb0ab6025226225a6ffb2fead5cd02728c
Mauro Moreno <moreno.mauro.emanuel@gmail.com>
2016-10-14 16:23:51 +0000

    Package name and DocBlocks

--- src/Provider/MercadoLibreServiceProvider.php
+++ src/Provider/MercadoLibreServiceProvider.php
@@ -1,14 +1,27 @@
 <?php
-
+/*
+ * This file is part of the Laravel Mercado Libre API client package.
+ *
+ * (c) Zephia <info@zephia.com.ar>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Zephia\LaravelMercadoLibre\Provider;
 
 use Illuminate\Support\ServiceProvider;
 use JMS\Serializer\SerializerBuilder;
 use Zephia\MercadoLibre\Client\MercadoLibreClient;
 
+/**
+ * Class MercadoLibreServiceProvider
+ *
+ * @package Zephia\LaravelMercadoLibre\Provider
+ * @author  Mauro Moreno <moreno.mauro.emanuel@gmail.com>
+ */
 class MercadoLibreServiceProvider extends ServiceProvider
 {
-    protected $packageName = '';
+    protected $packageName = 'MercadoLibre';
 
     /**
      * Bootstrap the application service
